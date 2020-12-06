94ca91cde911895820c3f89dd5a11fbe6e75051e
ojha <lipun.ojha838@outlook.com>
2020-12-06 12:31:32 +0530

    all added

---
+++ basic/basic.py
@@ -0,0 +1,17 @@
+'''
+os ex.
+playsound module
+and one multi string
+'''
+import os
+from playsound import playsound
+print('''Python is a multi-paradigm, "
+      "dynamically typed, multipurpose programming language. "
+      "It is designed to be quick to learn, understand,"
+      " and use, and enforce a clean and uniform syntax."
+      " Please note that Python 2 is officially out of support as of 01-01-2020. "
+      "Still, for version-specific Python questions, add the [python-2.7] or [python-3.x] tag."
+      " When using a Python variant (e.g., Jython, PyPy) or library (e.g., "
+      "Pandas and NumPy), please include it in the tags ''')
+playsound('//home//ojha//PycharmProjects//python_3//basic//music.mp3')
+print(os.getcwd())
